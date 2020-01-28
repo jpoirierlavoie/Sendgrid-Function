3145e8a456ae8b9a90dc209c12009b85e5aaeea8
Jason Poirier Lavoie <jpoirierlavoie@gmail.com>
2020-01-27 21:09:08 -0500

    Update main.py
    Added CORS headers

--- main.py
+++ main.py
@@ -25,7 +25,22 @@ def recaptcha_validation(request):
 
 def sendgrid_function(request):
 
-    if request.method == "POST":
+    if request.method == "OPTIONS":
+
+        headers = {
+            'Access-Control-Allow-Origin': os.environ["CONTACT_FORM_URI"],
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+        return ('', 204, headers)
+
+    elif request.method == "POST":
+
+        headers = {
+            'Access-Control-Allow-Origin': os.environ["CONTACT_FORM_URI"],
+        }
+
         message = {
             "personalizations": [
                 {
@@ -53,9 +68,14 @@ def sendgrid_function(request):
                 }
             ]
         }
+
         sg = sendgrid.SendGridAPIClient(os.environ["SENDGRID_API_KEY"])
         response = sg.send(message)
+
         if response.status_code == 202:
-            return "Email sent successfully."
-        return "Status Code: " + str(response.status_code)
-    return "Invalid Method. Only POST methods are accepted."
+            return ("Email sent successfully.", 200, headers)
+        else:
+            return ("Something went wrong. Status Code: " + str(response.status_code), headers)
+
+    else:
+        return "Invalid Method. Only POST methods are accepted."
